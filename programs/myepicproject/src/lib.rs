e533e9b860d35c661c1c80a15dfa74efdb9666d1
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-11-11 01:28:37 -0600

    Sets up attribute macros

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -3,3 +3,7 @@ use anchor_lang::prelude::*;
 
 // anchor generated "program id" for Solana to read
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+
+// calls Solana program to run module
+#[program]
