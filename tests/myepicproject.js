62b1a1a47deaceca7e39beef60796a32cd0063d3
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-11-11 04:00:36 -0600

    Retrieves account data

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,14 +1,34 @@
 const anchor = require('@project-serum/anchor');
 
+// Need the system program
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("🚀 Starting test...")
-
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.Provider.env();
   // tells anchor to automatically compile our code and deploy locally
   anchor.setProvider(anchor.Provider.env());
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+ 
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
 }
 
 // main func will run async
