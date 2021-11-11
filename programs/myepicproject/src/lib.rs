233f00b50904bbd3aef7999447a856376e27f6df
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-11-11 04:07:45 -0600

    Builds a function to update GIF counter

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -18,6 +18,14 @@ pub mod myepicproject {
       base_account.total_gifs = 0;
       Ok(())
     }
+
+        // Another function woo!
+    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+        // Get a reference to the account and increment total_gifs.
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+        Ok(())
+    }
   }
 // attribute macro that will specify different account constraints
 #[derive(Accounts)]
@@ -28,6 +36,14 @@ pub struct StartStuffOff<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program <'info, System>,
+}
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
 }
 
 
