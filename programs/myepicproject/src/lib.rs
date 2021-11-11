60a80a268c3749e23d01855b76110ef0c259816f
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-11-11 01:29:32 -0600

    Defines attribute macro

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -7,3 +7,11 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 // calls Solana program to run module
 #[program]
+
+// defines attribute macros
+pub mod myepicproject {
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+      Ok(())
+    }
+  }
